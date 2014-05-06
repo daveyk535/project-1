aaf8c045e3435985ba240a3b5500126c444923b4
daveyk535 <dave@phase2minds.com>
2014-05-06 14:35:39 -0700

    Finished basic signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  
   def new
     @user = User.new
   end
@@ -6,4 +7,21 @@ class UsersController < ApplicationController
   def show 
     @user = User.find(params[:id])
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to BurgerBuddy!"
+      redirect_to @user
+    else
+      flash[:error] = "Failed to create account.  Try again."
+      render 'new'
+    end
+  end
 end
+
+
+private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :twitter, :password, :password_confirmation)
+    end
