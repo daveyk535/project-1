0550e2bc10f44749352f5c1a246884f6403ffc69
daveyk535 <dave@phase2minds.com>
2014-05-06 09:57:48 -0700

    Continuing authentication. Ch. 6 Hartl complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140506144615) do
+ActiveRecord::Schema.define(version: 20140506142414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,8 +66,6 @@ ActiveRecord::Schema.define(version: 20140506144615) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
-    t.string   "password"
-    t.string   "password_confirmation"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
