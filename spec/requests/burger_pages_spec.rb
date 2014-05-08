6b4ac92145089c19cbb83fad888c2c3504097186
daveyk535 <dave@phase2minds.com>
2014-05-08 16:59:10 -0700

    ready for Heroku

--- spec/requests/burger_pages_spec.rb
+++ spec/requests/burger_pages_spec.rb
@@ -3,7 +3,26 @@ require 'spec_helper'
 describe "BurgerPages" do
   it 'gets a success response from the home page' do
     get '/'
-    expect(response.status).to eq(200)
+    expect(response).to eq(200)
   end
 end
 
+# describe "Todos Management" do 
+
+#   describe 'creating a new todo' do
+#     it 'renders form and creates todo' do
+#       get '/todos/new'
+#       expect(response).to render_template(:new)
+
+#       post "/todos", todo: {title: "My Todo", description: "My description"}
+
+#       expect(response).to redirect_to( assigns(:todo) )
+#       follow_redirect!
+
+#       expect(response).to render_template(:show)
+#       expect(response.body).to include("My Todo", "My description")
+#     end
+# end
+
+  
+# end
