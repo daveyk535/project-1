85dd331c8d9614ad8c88a80093b024db0d2646d8
daveyk535 <dave@phase2minds.com>
2014-05-08 18:48:31 -0700

    added link to current user for review

--- spec/requests/burger_pages_spec.rb
+++ spec/requests/burger_pages_spec.rb
@@ -6,23 +6,3 @@ describe "BurgerPages" do
     expect(response).to eq(200)
   end
 end
-
-# describe "Todos Management" do 
-
-#   describe 'creating a new todo' do
-#     it 'renders form and creates todo' do
-#       get '/todos/new'
-#       expect(response).to render_template(:new)
-
-#       post "/todos", todo: {title: "My Todo", description: "My description"}
-
-#       expect(response).to redirect_to( assigns(:todo) )
-#       follow_redirect!
-
-#       expect(response).to render_template(:show)
-#       expect(response.body).to include("My Todo", "My description")
-#     end
-# end
-
-  
-# end
