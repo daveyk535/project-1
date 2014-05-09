59bf1b8b3f84878670531e8d300714a151fb83be
daveyk535 <dave@phase2minds.com>
2014-05-08 17:24:56 -0700

    blah

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,15 +14,15 @@ users = User.create(first_name: "David", last_name: "Korsunsky", email: "davekor
 
 
 
-burgers = Burger.create(burger_name: 'le grande', price:20.00, protein: 'grass-fed proprietary', bun_type: 'Brioche', grass_fed: true, gluten_free: false, description: 'Grass-fed proprietary grind, Marin brie, roasted onion, smoked bacon, brioche', special_notes: 'Add sunny-side-up Petaluma egg for $2', restaurant_id: 1)
+burgers = Burger.create(burger_name: 'le grande', price:20.00, protein: 'grass-fed proprietary', bun_type: 'Brioche', grass_fed: true, gluten_free: false, description: 'Grass-fed proprietary grind, Marin brie, roasted onion, smoked bacon, brioche', special_notes: 'Add sunny-side-up Petaluma egg for $2', average_score: 29.5, restaurant_id: 1)
 
-burgers = Burger.create(burger_name: 'Drive-in Cheeseburger', price:6.00, protein: 'certified angus', bun_type: 'Acme', grass_fed: false, gluten_free: false, description: '1/3 lb certified angus, double am cheese, griddled onions, pickles, lettuce, tomato, 1000 island, acme bun', special_notes: 'Crazy Daves Double $8.00', restaurant_id: 2)
+burgers = Burger.create(burger_name: 'Drive-in Cheeseburger', price:6.00, protein: 'certified angus', bun_type: 'Acme', grass_fed: false, gluten_free: false, description: '1/3 lb certified angus, double am cheese, griddled onions, pickles, lettuce, tomato, 1000 island, acme bun', special_notes: 'Crazy Daves Double $8.00',average_score: 28.0, restaurant_id: 2)
 
-burgers = Burger.create(burger_name: 'House Made Vegan Burger', price:11.00, protein: 'Vegan', description: 'Brick House Vegan', restaurant_id: 3)
+burgers = Burger.create(burger_name: 'House Made Vegan Burger', price:11.00, protein: 'Vegan', description: 'Brick House Vegan', average_score: 27.0, restaurant_id: 3)
 
-burgers = Burger.create(burger_name: 'Marlowe Burger', price:14.00, protein: 'Certified Angus', bun_type: 'Brioche', grass_fed: true, gluten_free: false, description: 'Carmelized onions, cheddar, bacon, horseradish aioli', special_notes: 'Consistently ranked in SF Top 10', restaurant_id: 4)
+burgers = Burger.create(burger_name: 'Marlowe Burger', price:14.00, protein: 'Certified Angus', bun_type: 'Brioche', grass_fed: true, gluten_free: false, description: 'Carmelized onions, cheddar, bacon, horseradish aioli', special_notes: 'Consistently ranked in SF Top 10', average_score: 26, restaurant_id: 4)
 
-burgers = Burger.create(burger_name: 'Prather Ranch Burger', price:18.00, protein: 'Certified Angus', bun_type: 'Sesame Seed Challah Bun', grass_fed: true, gluten_free: false, description: 'house cut fries & pickle, grilled sesame seed challah bun 13 ', special_notes: 'add white cheddar or Pt. Reyes blue, starbelly bacon, avocado or roasted onions for 2 each', restaurant_id: 5)
+burgers = Burger.create(burger_name: 'Prather Ranch Burger', price:18.00, protein: 'Certified Angus', bun_type: 'Sesame Seed Challah Bun', grass_fed: true, gluten_free: false, description: 'house cut fries & pickle, grilled sesame seed challah bun 13 ', special_notes: 'add white cheddar or Pt. Reyes blue, starbelly bacon, avocado or roasted onions for 2 each', average_score: 25.0, restaurant_id: 5)
 
 
 
