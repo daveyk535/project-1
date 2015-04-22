999cb1428c7bff24890d2ef958d7c792d77f005e
daveyk535 <dave@phase2minds.com>
2015-04-21 20:14:29 -0700

    Reviews are now saving the total

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -13,7 +13,11 @@ class ReviewsController < ApplicationController
   def create 
     @review = Review.new(review_params)
     @review.user = current_user
+    # binding.pry
+    @review.total = params["review"]["patty"].to_i + params["review"]["bun"].to_i + params["review"]["bun_patty_ratio"].to_i + params["review"]["toppings"].to_i + params["review"]["wow_factor"].to_i
+    binding.pry
     @review.save
+    binding.pry
     redirect_to review_path(@review)
   end
 
@@ -21,5 +25,5 @@ end
 
 private
     def review_params
-      params.require(:review).permit(:patty, :bun, :bun_patty_ratio, :toppings, :service, :side, :total, :comments)
+      params.require(:review).permit(:patty, :bun, :bun_patty_ratio, :toppings, :wow_factor, :total, :comments)
     end
