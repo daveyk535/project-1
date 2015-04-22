ce9f7eb00962912ad843fb5d69a33d014bb3ef80
daveyk535 <dave@phase2minds.com>
2015-04-22 16:42:59 -0700

    Total is working. Next calculate average and update attribute

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,26 @@
 class Review < ActiveRecord::Base
   belongs_to :burger
   belongs_to :user
+
+
+  def calc_total
+    burgers = Burger.all
+    burgers.each do |burger|
+      @total = 0
+      reviews = Review.where(burger_id: burger.id)
+        reviews.each do |review|
+          # puts review.total
+          @total += review.total.to_i
+        end
+        puts @total
+    end
+  end
+
 end
+
+
+# reviews = Review.all
+#         reviews.each do |review|
+#           total = 0
+#           if review.burger_id
+#         end
--- config/routes.rb
+++ config/routes.rb
@@ -17,19 +17,3 @@ BurgerBuddy::Application.routes.draw do
   
 end
 
-
-Reports:
-
-We need x, y, z
-Last Mamogram
-
-
-What is your med list?
-What are you allergies?
-Last routine screening/annual exam?
-What is you blood type?
-
-
-Reports for physicians
-Diabetics
-Summarize
