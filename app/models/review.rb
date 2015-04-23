adc964ce173ec73b85a5c55bd1f7565526ba8d22
daveyk535 <dave@phase2minds.com>
2015-04-22 17:26:36 -0700

    Average score is working and updating. Now need to update data type for average_score so it shows as decimal

--- app/models/review.rb
+++ app/models/review.rb
@@ -6,21 +6,18 @@ class Review < ActiveRecord::Base
   def calc_total
     burgers = Burger.all
     burgers.each do |burger|
-      @total = 0
+      total = 0
+      average = 0
+      count = Review.where(burger_id: burger.id).count
       reviews = Review.where(burger_id: burger.id)
         reviews.each do |review|
           # puts review.total
-          @total += review.total.to_i
+          total += review.total.to_f
+          average = (total / count).to_f
         end
-        puts @total
+        puts average
+        burger.update_attributes(average_score: average)
     end
   end
 
 end
-
-
-# reviews = Review.all
-#         reviews.each do |review|
-#           total = 0
-#           if review.burger_id
-#         end
