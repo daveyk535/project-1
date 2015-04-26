8d17bbf2dc518457df80ae89378f408a39328c43
daveyk535 <dave@phase2minds.com>
2015-04-26 12:23:02 -0700

    secrets

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,6 +9,6 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-BurgerBuddy::Application.config.secret_token = aac7ae0d44d3bbb04112f37aeb999cea3d6c18aba450505ae6bd9a8360ed1359940831d81e94602298e0600a6211dbafdc19a269579419756e0f06e98b1a49a7
+BurgerBuddy::Application.config.secret_token = ENV['SECRET_KEY_BASE']
 # BurgerBuddy::Application.config.secret_base = ENV['SECRET_KEY_BASE']
-BurgerBuddy::Application.config.secret_key_base = aac7ae0d44d3bbb04112f37aeb999cea3d6c18aba450505ae6bd9a8360ed1359940831d81e94602298e0600a6211dbafdc19a269579419756e0f06e98b1a49a7
+BurgerBuddy::Application.config.secret_key_base = ENV["SECRET_KEY_BASE"]
