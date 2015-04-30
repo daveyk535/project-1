f8a9dd3536758d87e911b5f599d9b8a804962ca3
daveyk535 <dave@phase2minds.com>
2015-04-29 20:33:15 -0700

    Remove database from .env file

--- app/models/review.rb
+++ app/models/review.rb
@@ -10,7 +10,6 @@ class Review < ActiveRecord::Base
       count = Review.where(burger_id: burger.id).count
       reviews = Review.where(burger_id: burger.id)
         reviews.each do |review|
-          # puts review.total
           total += review.total.to_f
           average = (total / count).round(1).to_f
         end
